06fc1339544a0683f80556c5252c876657125b2f
lxi <75556084+lxi1400@users.noreply.github.com>
2023-02-15 00:36:16 -0600

    Start artist.GetInfo

--- user.go
+++ user.go
@@ -316,3 +316,89 @@ func (lfm *LastFM) GetUserTopArtists(user string, period Period, limit int) (top
 	}
 	return
 }
+
+type ArtistInfo struct {
+	Name string `xml:"name"`
+	MBID string `xml:"mbid"`
+	URL string `xml:"url"`
+	Image struct {
+	  SizeSmall string `xml:"small"`
+	  SizeMedium string `xml:"medium"`
+	  SizeLarge string `xml:"large"`
+	  SizeExtraLarge string `xml:"extralarge"`
+	  SizeMega string `xml:"mega"`
+	  Size string `xml:""`
+	} `xml:"image"`
+	Streamable int `xml:"streamable"`
+	Ontour int `xml:"ontour"`
+	Stats struct {
+	  Listeners int `xml:"listeners"`
+	  Playcount int `xml:"playcount"`
+	  Userplaycount int `xml:"userplaycount"`
+	} `xml:"stats"`
+}
+
+// Gets information for a Artist. The user argument can either be empty ("") or specify a last.fm username, in which
+// case .UserPlaycount will be valid in the returned struct. The autocorrect parameter controls whether
+// last.fm's autocorrection algorithms should be run on the artist name.
+//
+// The Artist struct must specify either the MBID or Artist.Name.
+// Example literals that can be given as the first argument:
+//   lastfm.Artist{MBID: "mbid"}
+//   lastfm.Artist{Name: "Artist"}
+//
+// See http://www.last.fm/api/show/artist.getInfo.
+func (lfm *LastFM) GetArtistInfo(artist Artist, user string, autocorrect bool) (info *ArtistInfo, err error) {
+	method := "artist.getInfo"
+	query := map[string]string{}
+	if autocorrect {
+		query["autocorrect"] = "1"
+	} else {
+		query["autocorrect"] = "0"
+	}
+
+	if user != "" {
+		query["username"] = user
+	}
+
+	if track.MBID != "" {
+		query["mbid"] = artist.MBID
+	} else {
+		query["artist"] = artist.Name
+	}
+
+	if data, err := lfm.cacheGet(method, query); data != nil {
+		switch v := data.(type) {
+		case ArtistInfo:
+			return &v, err
+		case *ArtistInfo:
+			return v, err
+		}
+	} else if err != nil {
+		return nil, err
+	}
+
+	body, hdr, err := lfm.doQuery(method, query)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+
+	status := lfmStatus{}
+	err = xml.NewDecoder(body).Decode(&status)
+	if err != nil {
+		return
+	}
+	if status.Error.Code != 0 {
+		err = &status.Error
+		go lfm.cacheSet(method, query, err, hdr)
+		return
+	}
+
+	info = &status.ArtistInfo
+	err = info.unmarshalHelper()
+	if err == nil {
+		go lfm.cacheSet(method, query, info, hdr)
+	}
+	return
+}
